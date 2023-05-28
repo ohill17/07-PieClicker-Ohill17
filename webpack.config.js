7a1146f1c0624d2a17a589bda0e466d452ae5b9b
Orion Hill <Orion123333@gmail.com>
2023-05-28 10:44:58 -0700

    Signed-off-by: Orion Hill <Orion123333@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -16,100 +16,111 @@ const isProduction = (process.env.NODE_ENV === 'production');
 /**
 * Include hash to filenames for cache busting - only at production
 */
-const fileNamePrefix = isProduction ? '[chunkhash].' : '';
+const fileNamePrefix = isProduction? '[chunkhash].' : '';
 
 module.exports = {
-  mode: !isProduction ? 'development' : 'production',
-  entry: {
-    main: './src/js/main.js',
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: fileNamePrefix + '[name].js',
-    assetModuleFilename: "assets/[name][ext]",
-    clean: true,
-  },
-  target: 'web',
-  devServer: {
-    static: "./dist"
-  },
-  /* no separate source map files in production */
-  devtool: !isProduction ? 'source-map' : 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/i,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
+    mode: !isProduction ? 'development': 'production',
+    entry: {
+      home: './src/js/home.js',
+      about: './src/js/about.js',
+      status: './src/js/status.js',
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: fileNamePrefix + '[name].js',
+      assetModuleFilename: "assets/[name][ext]",
+      clean: true,
+    },
+    target: 'web',
+    devServer: { 
+      static: "./dist"
+    }, 
+    /* no separate source map files in production */
+    devtool: !isProduction ? 'source-map' : 'inline-source-map', 
+    module: {
+      rules: [	
+        { 
+          test: /\.js$/i,
+          exclude: /(node_modules)/,
+          use: { 
+            loader: 'babel-loader', 
+            options: {
             presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ""
-            }
-          },
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ""
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(svg|eot|ttf|woff|woff2)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
-      inject: "body",
-      filename: "index.html",
-    }),
-    new copyPlugin({
-      patterns: [
+          }}
+        }, 
+        { 
+          test: /\.css$/i, 
+          /* separate js code and css in production */
+          use: isProduction ?
+            [ MiniCssExtractPlugin.loader, 'css-loader']	:
+            [ 'style-loader', 'css-loader']		
+        },
+        { 
+            test: /.s[ac]ss$/i, 
+            use: isProduction ?
+              [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']	:
+              [ 'style-loader', 'css-loader' , 'sass-loader']		
+        },
+        {  
+          test: /\.(svg|eot|ttf|woff|woff2)$/i,  
+          type: "asset/resource",
+        },
         {
-          from: path.resolve(__dirname, "src/assets/images"),
-          to: path.resolve(__dirname, "dist/assets/images"),
+          test: /\.(png|jpg|gif)$/i,
+          type: "asset/resource",
         },
       ],
-    }),
-    /* app uses global SERVER_URL rather than process.env.SERVER_URL */
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      SERVER_URL: JSON.stringify(process.env.SERVER_URL),
-      GMAP_KEY: JSON.stringify(process.env.GMAP_KEY),
-    }),
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: path.resolve(__dirname, "./src/index.html"),
+        chunks: ["home"],
+        inject: "body",
+        filename: "index.html",
+      }),
+      new htmlWebpackPlugin({
+        template: path.resolve(__dirname, "./src/about.html"),
+        chunks: ["about"],
+        inject: "body",
+        filename: "about.html",
+      }),
+      new htmlWebpackPlugin({
+        template: path.resolve(__dirname, "./src/status.html"),
+        chunks: ["status"],
+        inject: "body",
+        filename: "status.html",
+      }),
+      new copyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "src/assets/images"),
+            to: path.resolve(__dirname, "dist/assets/images"),
+          },
+        ],
+      }),
+      /* app uses global SERVER_URL rather than process.env.SERVER_URL */
+      new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        SERVER_URL: JSON.stringify(process.env.SERVER_URL),
+        GMAP_KEY: JSON.stringify(process.env.GMAP_KEY),
+      }),
+    ],
+    /* separates js (and css) that is shared between bundles - allows browser to cache */
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+}
+
+/**
+ * Production only plugins
+ */
+ if(isProduction) {
+  module.exports.plugins.push(
     new MiniCssExtractPlugin({
       filename: fileNamePrefix + "[name].css",
     })
-  ],
-  /* separates js (and css) that is shared between bundles - allows browser to cache */
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-  },
+  );
 };
+  
